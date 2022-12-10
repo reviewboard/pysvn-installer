2db3edbb8497515bf405d78a46ff441d51e0d9aa
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-10 23:29:50 +0000

    Adding tarfile member sanitization to extractall()

--- install.py
+++ install.py
@@ -105,7 +105,26 @@ def fetch_pysvn(pysvn_version):
 
 def extract_pysvn(tarball_path):
     with tarfile.open(tarball_path) as tar:
-        tar.extractall(temp_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, temp_path)
 
     try:
         return glob.glob(os.path.join(temp_path, 'pysvn-*'))[0]
